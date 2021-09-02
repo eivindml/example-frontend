eb6e1cf94a4c42e09b15e572ae558c0eea7f73d3
Eivind Lindbråten <eivindml@icloud.com>
2021-09-02 14:50:49 +0200

    Update package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import "shared/dist/index.css";
+import "@eivindml/shared/dist/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
